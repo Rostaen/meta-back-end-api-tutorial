b4617362dd10eca2c3e0b84b11cb3d29e6356db0
Andrew Fillhart <afillhar@bard.edu>
2024-01-18 11:22:42 -0500

    Finished W3 Ordering Video

--- BooklistAPI/views.py
+++ BooklistAPI/views.py
@@ -1,11 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
-from rest_framework import status
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, renderer_classes
 from rest_framework.views import APIView
 from .models import BookItem
 from .serializers import BookItemSerializer
-from django.shortcuts import get_object_or_404
+from rest_framework import status
 
 @api_view(['GET', 'POST'])
 def books(request):
@@ -14,11 +13,29 @@ def books(request):
 
 @api_view(['GET', 'POST'])
 def book_items(request):
-    if request.method == 'GET':
+    if request.method == 'GET': # GETting from the database
+        #variable declarations
         items = BookItem.objects.select_related('category').all()
+        category_name = request.query_params.get('category')
+        to_price = request.query_params.get('to_price')
+        search = request.query_params.get('search')
+        ordering = request.query_params.get('ordering')
+
+        # Filtered view of the database
+        if category_name:
+            items = items.filter(category__title = category_name)
+        if to_price:
+            items = items.filter(price__lte = to_price) # lte = less than or equal to
+        if search:
+            items = items.filter(title__icontains = search) #icontains will find the characters anywhere in the title
+        if ordering: # Currently there is a bug where multifield ordering is returning an error. Working on this bug.
+            ordering_fields = ordering.split(",")
+            items = items.order_by(*ordering_fields)
+
         serialized_item = BookItemSerializer(items, many=True)
         return Response(serialized_item.data)
-    if request.method == 'POST':
+
+    if request.method == 'POST': # Adding to the database
         serialized_item = BookItemSerializer(data=request.data)
         serialized_item.is_valid(raise_exception=True)
         serialized_item.save()
